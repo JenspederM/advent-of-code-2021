4ffab0fe05e84053b35c597ca41ef9075cc4b353
Jens Peder Meldgaard <jpd.ext@velux.com>
2021-12-03 13:06:48 +0100

    Finished solutino for day two

--- 2/solution.py
+++ 2/solution.py
@@ -1,2 +1,28 @@
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+file_path = "{}/input.txt".format(dir_path)
+
 if __name__ == "__main__":
-    pass
+    with open(file_path, "r") as f:
+        directions = [d.split("\n")[0] for d in f.readlines()]
+
+    x = 0
+    y = 0
+
+    for line in directions:
+        d, v = line.split(" ")
+
+        if d == "forward":
+            x += int(v)
+        elif d == "down":
+            y += int(v)
+        elif d == "up":
+            y -= int(v)
+        else:
+            raise KeyError("Direction {} is not supported".format(d))
+
+    print("Solution:")
+    print()
+    print("x = {}, y = {}".format(x, y))
+    print("x*y = {}".format(x * y))
